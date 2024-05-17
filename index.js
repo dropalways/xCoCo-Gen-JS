39afdbc6e03d304008b2f397c36366072301e01e
always <120043514+dropalways@users.noreply.github.com>
2024-05-17 21:16:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 function generateUsername(length) {
     let result = '';
 
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    // const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const  characters = "123456789";
     const charactersLength = characters.length;
 
     let counter = 0;
@@ -21,7 +22,7 @@ function generateUsername(length) {
 
 class Logger {
     static print_logo() {
-        console.log(gradient("blue", "cyan")(`
+        console.log(gradient("violet", "purple")(`
          ▄████▄  ▒█████   ▄████▄  ▒█████         ▄████ ▓█████ ███▄    █ 
         ▒██▀ ▀█ ▒██▒  ██▒▒██▀ ▀█ ▒██▒  ██▒    ▒ ██▒ ▀█▒▓█   ▀ ██ ▀█   █ 
         ▒▓█    ▄▒██░  ██▒▒▓█    ▄▒██░  ██▒    ░▒██░▄▄▄░▒███  ▓██  ▀█ ██▒
@@ -35,15 +36,15 @@ class Logger {
     }
 
     static info(message) {
-        console.log(gradient('green', 'yellow')("Info | " + message))
+        console.log(gradient("violet", "purple")("Info | " + message))
     }
 
     static warning(message) {
-        console.log(gradient('orange', 'yellow')("Warning | " + message))
+        console.log(gradient('yellow', 'lightyellow')("Warning | " + message))
     }
 
     static error(message) {
-        console.log(gradient('red', 'yellow')("Error | " + message))
+        console.log(gradient('red', 'firebrick')("Error | " + message))
     }
 }
 
@@ -124,7 +125,13 @@ async function main() {
     const [cardNumber, cardExpMonth, cardExpYear, cardCvv] = cardVCC.split('|')
 
     if (!cardNumber || !cardExpMonth || !cardExpYear || !cardCvv) {
-        return Logger.error("Invalid cc")
+        return Logger.error("Yo put sum shit in `vcc.txt`")
+    }
+    if (!email || !password) {
+        return Logger.error("Yo put sum shit in `combolist.txt`")
+    }
+    if (!offerId) {
+        return Logger.error("Yo put sum shit in `codes.txt`")
     }
 
     let url = "https://www.xbox.com/en-US/xbox-game-pass/invite-your-friends/redeem?offerId=";
@@ -144,36 +151,59 @@ async function main() {
         Logger.error("Failed to open browser")
     }
 
+
     await page.goto(url);
     await page.getByLabel('Sign in or create a Microsoft').click();
+    Logger.info("Clicked on Sign in or create a Microsoft Account")
     await page.getByTestId('i0116').fill(email);
+    Logger.info("Successfully put in the email from combolist.txt")
     await page.getByTestId('i0116').press('Enter');
     await page.getByTestId('i0118').fill(password);
+    Logger.info("Succesfully put in the password for the account")
     await page.getByTestId('i0118').press('Enter');
     await page.getByLabel('Stay signed in?').click();
-    // if (await page.getByText('Sorry, you are not eligible for this offer.See terms')) return console.log("You arent eligible(Maybe have gamepass already?)")
+
+    // if (await page.getByText('Sorry, you are not eligible for this offer.See terms')) return console.log("You aren't eligible(Maybe have gamepass already?)")
     await page.getByRole('button', { name: 'REDEEM NOW' }).click();
+    Logger.info("Clicked on Redeem Now")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByRole('button', { name: 'Get Started! Add a way to pay.' }).click();
+    Logger.info("Clicked on Debit or Credit Card option")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Choose a way to pay.Visa').click();
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Card number*').click();
+    Logger.info("Successfully put in the card number from vcc.txt")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Card number*').fill(cardNumber); // card number
+
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Cardholder Name*').click();
-    await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Cardholder Name*').fill('X'); // card holder name
+    Logger.info("Succesfully set a name for the cardholder name")
+    await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Cardholder Name*').fill('X'); // cardholder name
+
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Expiration month*').selectOption(cardExpMonth); // expiry month
+    Logger.info("Successfully added the expiry month")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Expiration year*').selectOption(cardExpYear); // expiry year
+    Logger.info("Successfully added the expiry year")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Save').click();
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('CVV*').click();
+    Logger.info("Successfully put in the cvv")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('CVV*').fill(cardCvv);
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Address line 1*').click();
+    Logger.info("Successfully put in a random address")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Address line 1*').fill(address); // address
+
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('City*').click();
+    Logger.info("Successfully put in the city")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('City*').fill(city);
+
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('State*').selectOption(stateAbbreviated); // state but short thing
+    Logger.info("Successfully put in the correct state")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByPlaceholder('20001').click();
+    Logger.info("Successfully put in the correct zipcode")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByPlaceholder('20001').fill(zipcode); // zipcode
+
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByPlaceholder('20001').click();
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByPlaceholder('20001').press('Enter');
+    Logger.info("Pressed Enter")
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Save').click();
+    Logger.info("Clicked on the save button")
 
     try {
         await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByRole('button', { name: 'Add profile address' }).click();
@@ -187,17 +217,24 @@ async function main() {
         await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByLabel('Use this address').click()
     }
     catch (exception) {
-        Logger.error(exception.stack);
+        // Logger.error(exception.stack);
     }
 
     await page.frameLocator('iframe[name="redeem-sdk-hosted-iframe"]').getByRole('button', { name: 'Confirm' }).click();
+    Logger.info("Clicked on the confirm button")
     await page.waitForSelector('text=Welcome to PC Game Pass', { "state": "visible" });
+    Logger.info("Successfully applied gamepass!")
     // await page.getByRole('button', { name: 'REDEEM NOW' }).click();
     await page.goto('https://www.minecraft.net/en-us/login');
+    Logger.info("Redirected to minecraft.net/en-us/login")
     await page.getByTestId('MSALoginButtonLink').click();
+    Logger.info("Clicked on log in with microsoft account")
     await page.getByRole('link', { name: 'Profile Name' }).click();
+    Logger.info("Clicked on set profile name")
     await page.getByTestId('profile-name-input').click();
-    await page.getByTestId('profile-name-input').fill(generateUsername(16));
+    // await page.getByTestId('profile-name-input').fill(generateUsername(16));
+    await page.get('profile-name-input').fill(`CocoGEN${generateUsername(9)}`)
+    Logger.info("Successfully set username check `hits.txt`")
 
     try {
         await page.getByTestId('ChangeNameButton').click();
